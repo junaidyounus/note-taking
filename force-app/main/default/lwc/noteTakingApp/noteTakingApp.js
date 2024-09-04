99a6c80673ce7b330748f65d56657c7b7d8de100
Junaid Younus <soomroj24@gmail.com>
2024-09-04 09:05:02 +0500

    72. Adding Form to Modal Component

--- force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
+++ force-app/main/default/lwc/noteTakingApp/noteTakingApp.js
@@ -1,7 +1,33 @@
 import { LightningElement } from 'lwc';
 
+const DEFAULT_NOTE_FORM = {
+    Name:"",
+    Note_Description__c:""
+}
+
 export default class NoteTakingApp extends LightningElement {
     showModal = false
+    noteRecord = DEFAULT_NOTE_FORM;
+    formats = [
+        'font',
+        'size',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'list',
+        'indent',
+        'align',
+        'link',
+        'clean',
+        'table',
+        'header',
+        'color',
+    ];
+
+    get isFormInvalid(){
+        return !(this.noteRecord && this.noteRecord.Name && this.noteRecord.Note_Description__c)
+    }
 
     createNoteHandler(){
         this.showModal = true
@@ -9,5 +35,16 @@ export default class NoteTakingApp extends LightningElement {
 
     closeModalHandler(){
         this.showModal = false
+        this.noteRecord = DEFAULT_NOTE_FORM
+    }
+
+    changeHandler(event){
+        const {name,value} = event.target;
+        this.noteRecord = {...this.noteRecord, [name]:value};
+    }
+
+    formSubmitHandler(event){
+        event.preventDefault();
+        console.log("this.noteRecord", this.noteRecord);
     }
 }
